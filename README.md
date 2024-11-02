bf00906099928b1f5ba2535a6756e773b2429ec9
timl <lehmitztim@gmail.com>
2024-11-02 22:19:19 +0100

    added hyperland and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Dies ist meine Persöhnliche Nixos-Config 
+# Dies ist meine Persöhnliche Nixos-Config 
 
 * TODO: write a nice README
+
+
+## Other TODOS
+
+* [ ] hyprland
+* [ ] make a folderstructure
+* [ ] home-manager stuff
+* [ ] split up /configuration.nix
+* [ ] 
+* [ ] 
+* [ ] 
--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,9 @@
   #Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes"];
 
+  #Enable Hyprland
+  programs.hyprland.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
