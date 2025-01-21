72f229ea48b2e48621a8a2911ed909da4fc256c8
timl <lehmitztim@gmail.com>
2025-01-21 21:52:30 +0100

    added Debian partition to the bootloader

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.loader.systemd-boot.extraEntries."debian.conf" = ''
+      title Debian
+      efi   /efi/debian/grubx64.efi
+    '';
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
